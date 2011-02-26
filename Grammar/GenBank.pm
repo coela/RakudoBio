524b2ea7a1c31816a93dff08b5b547376f1bd6a1
coela <coela.st@gmail.com>
2011-02-26 21:47:04 +0900

    added GenBank Grammar

---
+++ Grammar/GenBank.pm
@@ -0,0 +1,35 @@
+use v6;
+
+grammar RakudoBio::Grammer::GenBank {
+  token TOP {
+    ^<header>
+      ^^'FEATURES'\N+\n
+      <feature>+
+      ^^'ORIGIN'\N+\n
+      <origin>
+      ^^ \/\/ 
+  }
+  token header {
+    ^^[<!before "FEATURES">.]+
+  }
+
+  regex feature {
+    ^^"     "$<feature_type>=[\S+]\s+$<position>=[\S+'..'\S+]\n
+      <annotation>+
+  }
+  token annotation {
+    <annotation_head>
+      <annotation_continued>*
+  }
+  token annotation_head {
+    ^^'                     /'$<annotation_type>=[<!before \=>.]+'='$<text>=[\N+]\n
+  }
+
+  token annotation_continued {
+    ^^'                     '$<text>=[[<!before \/>.]\N+]\n
+  }
+  token origin {
+    [^^\s+\d+\s+$<seq>=[\N+]\n]+
+  }
+}
+
